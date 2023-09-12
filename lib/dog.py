8dc2facfbe6c0bab7be96db7ee441b661dca6e7a
+ <+>
2023-09-12 14:54:31 -0600

    finishe lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+fido = Dog("Fido")
+fido.breed
+        
+
+
+
+#Define a Dog class in lib/dog.py. In the class,
+#define an __init__ method that accepts an argument for the dog's name. 
+#That argument should be stored within a self.name attribute.
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, person="Person"):
+        self.name = person  
+        
+    
+    
+    
+#Define a Person class in lib/person.py.
+# In the class, define an __init__ method that accepts an argument for the person's name. 
+# That argument should be stored within a self.name attribute.
